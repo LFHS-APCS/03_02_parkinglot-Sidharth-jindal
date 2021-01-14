56c8f9349b58a9a8b1392ff6c535b9674f455ea7
Sidharth-jindal <69948951+Sidharth-jindal@users.noreply.github.com>
2021-01-13 22:09:08 -0600

    Update ParkingLot.java

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,1 +1,29 @@
-
+public class ParkingLot{
+  private int numCars;
+  public final int MAX_CARS = 2500;
+  public ParkingLot(){
+    numCars = 0;
+  }
+  public ParkingLot(int theNumCars){
+    numCars = theNumCars;
+  }
+  public int getNumCars(){
+    return numCars;
+  }
+  public void setNumCars(int theNumCars){
+    if(theNumCars <= MAX_CARS && theNumCars >= 0){
+      numCars = theNumCars;
+    }
+  }
+  public String toString(){
+    return "Number of cars is " + numCars;
+  }
+  public boolean equals(Object other){
+    ParkingLot p = (ParkingLot)other;
+    if(p.getNumCars() == numCars){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
